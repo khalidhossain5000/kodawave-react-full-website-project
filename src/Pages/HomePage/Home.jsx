887317270d5231cee0f3c57450ac824a6ac088c9
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-04-07 09:31:32 +0600

    font family setup completed

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => {
     return (
         <div>
-            <h2 className='bg-green-600 py-6 text-center text-5xl font-bold'>Home Page</h2>
+            <h2 className='bg-green-600 py-6 text-center text-5xl font-bold font-grotesk'>Home Page</h2>
         </div>
     );
 };
